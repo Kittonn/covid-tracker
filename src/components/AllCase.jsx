5945c7e16998bede9444aba60497a14406d75588
Kitton._ <tonkittipod@gmail.com>
2022-04-04 20:47:47 +0700

    All Case

--- src/components/AllCase.jsx
+++ src/components/AllCase.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCase } from "../store/covidAllCaseSlice";
+import { FcPlus } from "react-icons/fc";
+
 const AllCase = () => {
   const {
     update_date,
@@ -18,11 +20,42 @@ const AllCase = () => {
   }, [dispatch]);
 
   return (
-    <div className="bg-[#e5e5e5] ">
+    <div className="bg-[#e5e5e5] font-[Poppins]">
       <div className="w-[90%] lg:w-[80%] xl:w-[60%] mx-auto">
-        <div>
-          <p>Confirmed Case</p>
-          <h1>{}</h1>
+        <div className="flex flex-wrap justify-between ">
+          <div className="bg-white flex-1 m-3 p-5 rounded-xl shadow-md">
+            <p className="text-lg whitespace-nowrap">Confirmed Case</p>
+            <h1 className="text-2xl font-bold text-[#03045e] tracking-widest">
+              {total_case}
+            </h1>
+            <hr />
+            <div className="flex items-center">
+              <FcPlus />
+              <div>{new_case}</div>
+            </div>
+          </div>
+          <div className="bg-white flex-1 m-3 p-5 rounded-xl shadow-md">
+            <p className="text-lg whitespace-nowrap">Recovered Case</p>
+            <h1 className="text-2xl font-bold text-[#03045e] tracking-widest">
+              {total_recovered}
+            </h1>
+            <hr />
+            <div className="flex items-center">
+              <FcPlus />
+              <div>{new_recovered}</div>
+            </div>
+          </div>
+          <div className="bg-white flex-1 m-3 p-5 rounded-xl shadow-md">
+            <p className="text-lg whitespace-nowrap">Death Case</p>
+            <h1 className="text-2xl font-bold text-[#03045e] tracking-widest">
+              {total_death}
+            </h1>
+            <hr />
+            <div className="flex items-center">
+              <FcPlus />
+              <div>{new_death}</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
