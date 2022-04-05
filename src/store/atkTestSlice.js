435c29573a0909bd9476bcb7889da4fb18a1cf34
Kitton._ <tonkittipod@gmail.com>
2022-04-05 14:21:31 +0700

    Add AtkCase

---
+++ src/store/atkTestSlice.js
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url =
+  "https://raw.githubusercontent.com/wiki/porames/the-researcher-covid-data/cases/testing-data.json";
+
+export const getTest = createAsyncThunk("/cases/getTest", async () => {
+  return axios.get(url).then((res) => res.data);
+});
+
+const atkTestSlice = createSlice({
+  name: "atktest",
+  initialState: {
+    list: [],
+    status: null,
+  },
+  extraReducers: {
+    [getTest.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [getTest.fulfilled]: (state, action) => {
+      state.list = action.payload;
+      state.status = "success";
+    },
+    [getTest.rejected]: (state, action) => {
+      state.status = "failed";
+    },
+  },
+});
+
+export const getTestActions = atkTestSlice.actions;
+
+export default atkTestSlice;
