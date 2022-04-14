145e02c4097489a083026b32a58b959aad57469c
Kitton._ <tonkittipod@gmail.com>
2022-04-14 10:39:08 +0700

    Add package

--- src/data/type.js
+++
@@ -1,5 +0,0 @@
-export const type = [
-  { id: 1, text: "ผู้ติดเชื้อรายใหม่", value: "new_case" },
-  { id: 2, text: "ผู้เสียชีวิตรายใหม่", value: "new_death" },
-  { id: 3, text: "ผู้ป่วยรักษาหาย", value: "new_recovered" },
-];
--- src/data/vaccinegraphtype.js
+++
@@ -1,4 +0,0 @@
-export const vaccinegraphtype = [
-  { id: 1, text: "แยกตามเข็มที่ฉีด", value: "dose_type" },
-  { id: 2, text: "แยกตามชื่อผู้ผลิต", value: "brand_type" },
-];
